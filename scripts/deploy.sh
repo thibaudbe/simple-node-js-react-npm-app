d3be561e72c3a6a432b342bfbcb42e19c51e3844
Thibaud B <thibaudb.com@gmail.com>
2018-02-07 09:46:52 +0100

    core: docker-compose without detached arg

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 if [ ! -d "simple-node-js-react-npm-app" ]; then
   git clone "git@github.com:thibaudbe/simple-node-js-react-npm-app.git" "simple-node-js-react-npm-app"
   cd simple-node-js-react-npm-app
-  docker-compose up -d
+  docker-compose up
 else
   cd simple-node-js-react-npm-app
   git fetch --all
   git reset --hard origin/master
-  docker-compose up -d --no-recreate
+  docker-compose up --no-recreate
 fi
