645e9afc1a71862fd6e8c0d2317eaa5498902787
Thibaud B <thibaudb.com@gmail.com>
2018-02-07 09:16:27 +0100

    core: user git for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,10 @@
-git fetch --all
-git reset --hard origin/master
-docker-compose restart
+if [ ! -d "$FOLDER" ]; then
+  git clone "$REPO_URL" "$FOLDER"
+  cd $FOLDER
+  docker-compose up
+else
+  cd $FOLDER
+  git fetch --all
+  git reset --hard origin/master
+  docker-compose restart
+fi
