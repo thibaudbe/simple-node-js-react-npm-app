792fc17efdcc4b7c69aa2c0ade5bd3c5efe5e269
Thibaud B <thibaudb.com@gmail.com>
2018-02-07 09:41:54 +0100

    core: update docker-compose cmd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 if [ ! -d "simple-node-js-react-npm-app" ]; then
   git clone "git@github.com:thibaudbe/simple-node-js-react-npm-app.git" "simple-node-js-react-npm-app"
   cd simple-node-js-react-npm-app
-  docker-compose up
+  docker-compose up -d
 else
   cd simple-node-js-react-npm-app
   git fetch --all
   git reset --hard origin/master
-  docker-compose restart
+  docker-compose up -d --no-recreate
 fi
