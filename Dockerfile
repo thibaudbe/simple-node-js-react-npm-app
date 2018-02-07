37d1c46eb10a4b400eed7a4843a915879d7bccc4
Thibaud B <thibaud.boucherie@fabernovel.com>
2018-02-07 11:40:49 +0100

    core: add build process for docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM node:alpine
 
 WORKDIR app/
-COPY package.json .
 
+COPY package.json .
 # RUN rm /etc/nginx/conf.d/test.conf
 COPY ./scripts/nginx/test.conf /etc/nginx/conf.d/
 # COPY ./scripts/nginx/ /etc/nginx/vhost.d/
-
 RUN npm install --quiet
 
 COPY . .
+
+EXPOSE 3000
+
+CMD npm start
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 if [ ! -d "simple-node-js-react-npm-app" ]; then
-  git clone "git@github.com:thibaudbe/simple-node-js-react-npm-app.git" "simple-node-js-react-npm-app"
+  git clone git@github.com:thibaudbe/simple-node-js-react-npm-app.git
   cd simple-node-js-react-npm-app
-  docker-compose up
+  docker-compose up -d
 else
   cd simple-node-js-react-npm-app
   git fetch --all
   git reset --hard origin/master
-  docker-compose up --no-recreate
+  docker-compose up -d --no-recreate
 fi
